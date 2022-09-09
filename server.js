b7bf1c37d8b63a861ced24699619c75f52f23c01
goldsbs <55133653+goldsbs@users.noreply.github.com>
2022-09-09 08:47:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -127,6 +127,9 @@ app.get('/getCareerHistory', function(request, response) {
               result=data.filter( element => element.TITLE_TYPE =="video");
           if (result.length == 0)
               result={success:-2,message:"No data foundd for this search"};
+          if (result.length > 20) {
+              result=result.splice(21,(result.length-20));
+          }
           console.log(result);
           return response.json({data:result});
  //         return response.json({data:data});
