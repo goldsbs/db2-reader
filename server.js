17c83bd9a4d24f450169d2dc68a144f678171d3a
goldsbs <55133653+goldsbs@users.noreply.github.com>
2022-09-09 10:18:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ app.get('/getCareerHistory', function(request, response) {
   //        if (result.length > 2) {
   //            result=result.splice(2,2);
   //        }
-          result=data.filter( element => element.START_YEAR >1990);
+          result=data.filter( element => element.START_YEAR >2000);
           console.log(result);
           return response.json({data:result});
  //         return response.json({data:data});
