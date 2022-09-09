73f4c7182e40e552fcae9184c7244300d9955216
goldsbs <55133653+goldsbs@users.noreply.github.com>
2022-09-09 14:48:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -133,7 +133,8 @@ app.get('/getCareerHistory', function(request, response) {
           result=data.filter( element => element.START_YEAR <1988);
           var text=JSON.stringify(result);
           text=text.replace(/!/g,"");
-          text=text.replace("null","\"Not Avail\"");
+          text=text.replace('null','"Not Avail"');
+          text=text.replace('""','"Not Avail"');
           result=JSON.parse(text);
           console.log(result);
           return response.json({data:result});
